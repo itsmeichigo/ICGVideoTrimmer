f9d16125fd1363204e8e76d50cb1d33873b9b6a4
Huong Do <huongdt29@gmail.com>
2015-01-25 01:48:10 +0700

    Complete demo

--- ICGVideoTrimmer/ViewController.m
+++ ICGVideoTrimmer/ViewController.m
@@ -11,9 +11,17 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate, ICGVideoTrimmerDelegate>
 
 @property (weak, nonatomic) IBOutlet ICGVideoTrimmerView *trimmerView;
+@property (weak, nonatomic) IBOutlet UIButton *trimButton;
+
+@property (strong, nonatomic) NSString *tempVideoPath;
+@property (strong, nonatomic) AVAssetExportSession *exportSession;
+@property (strong, nonatomic) AVAsset *asset;
+
+@property (assign, nonatomic) CGFloat startTime;
+@property (assign, nonatomic) CGFloat stopTime;
 
 @end
 
@@ -21,7 +29,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.tempVideoPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"tmpMov.mov"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,18 +38,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - ICGVideoTrimmerDelegate
+
+- (void)trimmerView:(ICGVideoTrimmerView *)trimmerView didChangeLeftPosition:(CGFloat)startTime rightPosition:(CGFloat)endTime
+{
+    self.startTime = startTime;
+    self.stopTime = endTime;
+}
+
+
+#pragma mark - UIImagePickerControllerDelegate
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     [picker dismissViewControllerAnimated:YES completion:nil];
     NSURL *url = [info objectForKey:UIImagePickerControllerMediaURL];
-    AVAsset *asset = [AVAsset assetWithURL:url];
+    self.asset = [AVAsset assetWithURL:url];
     [self.trimmerView setThemeColor:[UIColor lightGrayColor]];
-    [self.trimmerView setAsset:asset];
+    [self.trimmerView setAsset:self.asset];
     [self.trimmerView setShowsRulerView:YES];
+    [self.trimmerView setDelegate:self];
     [self.trimmerView resetSubviews];
+    
+    [self.trimButton setHidden:NO];
 }
 
-- (IBAction)selectAsset:(id)sender {
+
+#pragma mark - Actions
+
+- (void)deleteTempFile
+{
+    NSURL *url = [NSURL fileURLWithPath:self.tempVideoPath];
+    NSFileManager *fm = [NSFileManager defaultManager];
+    BOOL exist = [fm fileExistsAtPath:url.path];
+    NSError *err;
+    if (exist) {
+        [fm removeItemAtURL:url error:&err];
+        NSLog(@"file deleted");
+        if (err) {
+            NSLog(@"file remove error, %@", err.localizedDescription );
+        }
+    } else {
+        NSLog(@"no file by that name");
+    }
+}
+
+- (IBAction)selectAsset:(id)sender
+{
     UIImagePickerController *myImagePickerController = [[UIImagePickerController alloc] init];
     myImagePickerController.sourceType =  UIImagePickerControllerSourceTypePhotoLibrary;
     myImagePickerController.mediaTypes =
@@ -48,5 +93,64 @@
     myImagePickerController.delegate = self;
     myImagePickerController.editing = NO;
     [self presentViewController:myImagePickerController animated:YES completion:nil];
+}
+
+- (IBAction)trimVideo:(id)sender
+{
+    [self deleteTempFile];
+    
+    NSArray *compatiblePresets = [AVAssetExportSession exportPresetsCompatibleWithAsset:self.asset];
+    if ([compatiblePresets containsObject:AVAssetExportPresetMediumQuality]) {
+        
+        self.exportSession = [[AVAssetExportSession alloc]
+                              initWithAsset:self.asset presetName:AVAssetExportPresetPassthrough];
+        // Implementation continues.
+        
+        NSURL *furl = [NSURL fileURLWithPath:self.tempVideoPath];
+        
+        self.exportSession.outputURL = furl;
+        self.exportSession.outputFileType = AVFileTypeQuickTimeMovie;
+        
+        CMTime start = CMTimeMakeWithSeconds(self.startTime, self.asset.duration.timescale);
+        CMTime duration = CMTimeMakeWithSeconds(self.stopTime - self.startTime, self.asset.duration.timescale);
+        CMTimeRange range = CMTimeRangeMake(start, duration);
+        self.exportSession.timeRange = range;
+        
+        [self.exportSession exportAsynchronouslyWithCompletionHandler:^{
+            
+            switch ([self.exportSession status]) {
+                case AVAssetExportSessionStatusFailed:
+                    
+                    NSLog(@"Export failed: %@", [[self.exportSession error] localizedDescription]);
+                    break;
+                case AVAssetExportSessionStatusCancelled:
+                    
+                    NSLog(@"Export canceled");
+                    break;
+                default:
+                    NSLog(@"NONE");
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        NSURL *movieUrl = [NSURL fileURLWithPath:self.tempVideoPath];
+                        UISaveVideoAtPathToSavedPhotosAlbum([movieUrl relativePath], self,@selector(video:didFinishSavingWithError:contextInfo:), nil);
+                    });
+                    
+                    break;
+            }
+        }];
+        
+    }
+}
+
+- (void)video:(NSString*)videoPath didFinishSavingWithError:(NSError*)error contextInfo:(void*)contextInfo {
+    if (error) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+                                                       delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+                                                       delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
 }
 @end
